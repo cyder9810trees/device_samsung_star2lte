0247060c24de5478b619bad9d1329d664833e318
Cyder <phamnguyengiakhiem@gmail.com>
2023-06-09 16:23:17 +0700

    star2lte: Derp-ify

--- derp_star2lte.mk
+++ derp_star2lte.mk
@@ -27,13 +27,14 @@ $(call inherit-product, device/samsung/star2lte/device.mk)
 # Boot Animation
 TARGET_SCREEN_HEIGHT := 2960
 TARGET_SCREEN_WIDTH := 1440
-TARGET_BOOT_ANIMATION_RES := 1440
 
-## Inherit some common AOSP stuff
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+# Official-ify
+$(call inherit-product, vendor/derp/config/common_full_phone.mk)
+DERP_BUILDTYPE := Official
+USE_LEGACY_BOOTANIMATION := true
 
 ## Device identifier, this must come after all inclusions
-PRODUCT_NAME := aosp_star2lte
+PRODUCT_NAME := derp_star2lte
 PRODUCT_DEVICE := star2lte
 PRODUCT_BRAND := samsung
 PRODUCT_MODEL := SM-G965F
